f046c4ac6c35689e6781168382fb9d5c27bb57db
Kyle <dahaydnator@gmail.com>
2024-06-11 06:07:13 -0500

    fixed bug with importing Header/Footer components from lowercase file names

--- client/src/components/NavTabs.jsx
+++ client/src/components/NavTabs.jsx
@@ -1,7 +1,7 @@
-import { Link, useLocation } from 'react-router-dom';
+
 
 function NavTabs() {
-    const currentPage = useLocation().pathname;
+    
     return (
         <ul>
             <li>about me</li>
